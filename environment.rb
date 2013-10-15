ecf047909708aadc0e54d7bc55c9662eaba4e4dc
Greg Eng <greg.eng@gmail.com>
2013-10-15 18:30:20 -0400

    start library parser specs

--- environment.rb
+++ environment.rb
@@ -1,7 +1,10 @@
 #environment
 require 'pry'
+
+require_relative 'lib/models/library_parser'
 require_relative 'lib/models/artist'
 require_relative 'lib/models/song'
 require_relative 'lib/models/genre'
+
 
 require_relative 'spec/spec_helper'
--- lib/models/library_parser.rb
+++ lib/models/library_parser.rb
@@ -1,8 +1,20 @@
-Dir.foreach('../../data') do |item|
- next if item == "." || item == ".."
-   item.gsub!("[","- ").gsub!("]"," - ")
-   parsed = item.split(" - ")
-   artist << parsed[0]
-   song << parsed[1]
-   genre << parsed[2]
-end
+# class LibraryParser
+
+artist = []
+song = []
+genre = []
+
+  # Dir.foreach('../../data') do |item|
+  #  next if item == "." || item == ".."
+  #    item.gsub!("[","- ").gsub!("]"," - ")
+  #    parsed = item.split(" - ")
+  #    artist << parsed[0]
+  #    song << parsed[1]
+  #    genre << parsed[2]
+  # end
+
+# end
+
+p artist
+p song
+p genre
