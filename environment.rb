8c294439aa063e79b054ec2c5d6bb357e3b88e13
samueldowens <samueldowens@gmail.com>
2013-10-15 18:57:26 -0400

    added parser code.

--- environment.rb
+++ environment.rb
@@ -3,5 +3,6 @@ require 'pry'
 require_relative 'lib/models/artist'
 require_relative 'lib/models/song'
 require_relative 'lib/models/genre'
+require_relative 'lib/models/library_parser'
 
 require_relative 'spec/spec_helper'
--- lib/models/library_parser.rb
+++ lib/models/library_parser.rb
@@ -1,8 +1,26 @@
-Dir.foreach('../../data') do |item|
- next if item == "." || item == ".."
-   item.gsub!("[","- ").gsub!("]"," - ")
-   parsed = item.split(" - ")
-   artist << parsed[0]
-   song << parsed[1]
-   genre << parsed[2]
+class LibraryParser
+
+  def make_objects
+    artist = Artist.new.tap{|x| x.name = @parsed[0]}
+    genre = Genre.new.tap{|x| x.name = @parsed[2]; x.artists = artist}
+    song = Song.new.tap{|x| x.name = @parsed[1]; x.artist = artist; x.genre = genre}
+
+    artist.songs = song
+    artist.genres = genre
+    genre.songs = song
+  end
+
+  def get_and_split_array
+    Dir.foreach("data") do |item|
+       if item != "." && item != ".."
+        @parsed = item.gsub!("[","- ").gsub!("]"," - ").split(" - ")
+        make_objects
+      end
+    end
+  end
+
+
 end
+
+# x = LibraryParser.new
+# binding.pry
