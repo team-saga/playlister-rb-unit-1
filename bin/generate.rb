844063b42048326f8d839513f60ebbd87433bfd2
Anders Ramsay <andersr@gmail.com>
2013-10-16 15:40:05 -0400

    added artists and fixed binding error

--- bin/generate.rb
+++ bin/generate.rb
@@ -5,36 +5,36 @@ class SiteGenerator
 	x = LibraryParser.new
 	x.get_and_split_array
 
-	songs-index = ERB.new(File.open('lib/views/songs-index.erb').read)
+	songs_index = ERB.new(File.open('lib/views/songs_index.erb').read)
 	@songs = Song.all
 
-	File.open('_site/songs-index.html', 'w+') do |f|
-	  f << songs-index.result(binding)
+	File.open('_site/songs_index.html', 'w+') do |f|
+	  f << songs_index.result(binding)
 	end
 
-	song-show = ERB.new(File.open('lib/views/song-show.erb').read)
+	song_show = ERB.new(File.open('lib/views/song_show.erb').read)
 	# For each song, first, cast the song into instance var
 
 	@songs.each do |song|
 	   @song = song
 	  File.open("_site/songs/#{song.url}.html", 'w+') do |f|
-	    f << song-show.result
+	    f << song_show.result(binding)
 	  end
 	end
 
-	artists-index = ERB.new(File.open('lib/views/artists-index.erb').read)
+	artists_index = ERB.new(File.open('lib/views/artists_index.erb').read)
 	@artists = Artist.all
 
-	File.open('_site/artists-index.html', 'w+') do |f|
-	  f << artists-index.result(binding)
+	File.open('_site/artists_index.html', 'w+') do |f|
+	  f << artists_index.result(binding)
 	end
 
-	artist-show = ERB.new(File.open('lib/views/artist-show.erb').read)
+	artist_show = ERB.new(File.open('lib/views/artist_show.erb').read)
 
 	@artists.each do |artist|
 	   @artist = artist
 	  File.open("_site/artists/#{artist.url}.html", 'w+') do |f|
-	    f << artist-show.result
+	    f << artist_show.result(binding)
 	  end
 	end	
 
