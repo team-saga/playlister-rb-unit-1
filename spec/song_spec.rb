41029c590361ce00daef4e67ff0bab5e53ca3278
samueldowens <samueldowens@gmail.com>
2013-10-16 14:24:57 -0400

    updated tests

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,25 +1,25 @@
 require_relative './spec_helper'
 
-
 describe "Song" do
-	let(:song) {Song.new}
+
+  let(:new_song) {Song.new}
+
   it "can initialize a song" do
-    song.should be_an_instance_of(Song)
+   Song.new.should be_an_instance_of(Song)
   end
 
   it "can have a name" do
-    song.name = "1 + 1"
-    song.name.should eq("1 + 1")
+    new_song.name = ("Frank")
+    new_song.name.should eq ("Frank")
   end
 
   it "can have a genre" do
-    song.genre = "hip-hop"
-    song.genre.should eq("hip-hop")
+    new_song.genre = Genre.find_or_create_by_name("Rock")
+    new_song.genre.name.should eq ("Rock")
   end
 
   it "has an artist" do
-    song.artist = "Jay-Z & Kanye West"
-    song.artist.should eq("Jay-Z & Kanye West")
+   new_song.artist = Artist.find_or_create_by_name("Some Guy")
+   new_song.artist.name.should eq ("Some Guy")
   end
-
 end
